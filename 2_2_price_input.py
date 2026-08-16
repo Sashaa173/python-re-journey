40b6886b3bb58d61069567fa67c10fa1213ed26a
Aleksandra <sashaa170303@gmail.com>
2026-08-16 19:58:18 +0300

    refactor: meaninful variable names, sep insted of manual spacing

--- 2_2_price_input.py
+++ 2_2_price_input.py
@@ -1,5 +1,4 @@
-a = input()
-print("Стоимость квадратного метра" , "-" ,  a)
-b = input()
-print("Площадь квартиры" , "-" , b)
-
+price_per_m2 = input()
+print("Стоимость квадратного метра", "-", price_per_m2, sep=" ")
+area = input()
+print("Площадь квартиры", "-", area, sep=" ")
